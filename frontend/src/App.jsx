2d83e3aa39ed4658409a1fa13bbc6972e72d34f2
choong77 <speak2946@gmail.com>
2026-07-24 09:45:10 +0900

    git 포트 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-const API_BASE = "http://127.0.0.1:8000"
+const API_BASE = "http://127.0.0.1:8701"
 
 function App() {
   const [list, setList] = useState([])
