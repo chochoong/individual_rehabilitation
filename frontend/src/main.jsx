9c8c9176686f2497363fb85033ca98f7b06b3e48
rnfanf8401 <rnfanf8401@gmail.com>
2026-07-27 16:51:47 +0900

    update 충돌

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
 import InputPage from './input_service/InputPage.jsx'
-=======
 import App from './App.jsx'
->>>>>>> develop
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
